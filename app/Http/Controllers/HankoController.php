565501e603a77d60bb2c2b034263449fc4bfff7f
shiratsu <hiratsukashu@indival.co.jp>
2020-11-06 10:03:37 +0900

    ハンコ試作品

---
+++ app/Http/Controllers/HankoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HankoController extends Controller
+{
+    //
+    public function index ()
+    {
+
+        return view('hanko');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('hanko', 'HankoController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/hello', function () {
+    return PDF::loadHTML('<h1>Hello!</h1>')->inline();
+});
+
+Route::get('/hanko_pdf', function () {
+    // $member = Faker\Factory::create('ja_JP');
+    return PDF::loadView('hanko')->inline();
+});
